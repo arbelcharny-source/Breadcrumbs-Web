d3ebf7f116fb7ffda6b2e8e51b7e214c9fbbed0a
Arbel Charny <arbel.charny@gmail.com>
2026-03-01 19:49:06 +0200

    feat(client): Configured Google OAuth provider in app root

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import './index.css'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const GOOGLE_CLIENT_ID = "YOUR_GOOGLE_CLIENT_ID_APPS_GOOGLEUSERCONTENT_COM"; 
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+      <App />
+    </GoogleOAuthProvider>
+  </React.StrictMode>,
 )
